ea916f9bea6f36726ee5168a3732e8a9cf5cad2d
Ibnu Aziiz Wan Hanafi <2410631170025@student.unsika.ac.id>
2024-09-29 16:11:50 +0700

    Add files via upload

---
+++ Pembagian gaji.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+
+int pokok(int jamKerja) {
+    return jamKerja * 7500 ;
+}
+
+int lembur(int jamKerja) {
+    int jamLembur = jamKerja - 8;
+    if (jamLembur > 0) {
+        return jamLembur * 7500 * 1.5;
+    } else {
+        return 0;
+    }
+}
+
+int makan(int jamKerja) {
+    if (jamKerja >= 9) {
+        return 10000;
+    } else {
+        return 0;
+    }
+}
+
+int transport(int jamKerja) {
+    if (jamKerja >= 10) {
+        return 13000;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    string nip, nama;
+    int jamKerja, gajiPokok, gajiLembur, uangMakan, uangTransport, totalGaji;
+
+    cout << "Masukkan NIP: ";
+    cin >> nip;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    cout << "Masukkan Jumlah Jam Kerja: ";
+    cin >> jamKerja;
+
+    gajiPokok = pokok(jamKerja);
+    gajiLembur = lembur(jamKerja);
+    uangMakan = makan(jamKerja);
+    uangTransport = transport(jamKerja);
+    totalGaji = gajiPokok + gajiLembur + uangMakan + uangTransport;
+
+    cout << "\nRincian Gaji Karyawan" << endl;
+    cout << "NIP: " << nip << endl;
+    cout << "Nama: " << nama << endl;
+    cout << "Gaji Pokok: Rp " << gajiPokok << endl;
+    cout << "Gaji Lembur: Rp " << gajiLembur << endl;
+    cout << "Uang Makan: Rp " << uangMakan << endl;
+    cout << "Uang Transport: Rp " << uangTransport << endl;
+    cout << "Total Gaji: Rp " << totalGaji << endl;
+
+    return 0;
+}
